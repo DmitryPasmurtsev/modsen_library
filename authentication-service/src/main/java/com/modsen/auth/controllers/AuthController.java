96d68bddbeae1ee8149529cac3673d33528fd76a
DmitryPasmurtsev <dima427614@gmail.com>
2023-11-15 22:27:49 +0300

    added swagger annotations auth-service

--- authentication-service/src/main/java/com/modsen/auth/controllers/AuthController.java
+++ authentication-service/src/main/java/com/modsen/auth/controllers/AuthController.java
@@ -3,6 +3,8 @@ package com.modsen.auth.controllers;
 import com.modsen.auth.dtos.AuthRequest;
 import com.modsen.auth.dtos.AuthResponse;
 import com.modsen.auth.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@Tag(name = "Аутентификационный контроллер",description = "Предназначен для получения JWT токена")
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping(value = "/auth")
+    @Operation(
+            summary = "Получение JWT токена"
+    )
     public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
